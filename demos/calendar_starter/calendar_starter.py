0da6f7e668b8e2340117a86fec87fcf7eb0cfd6a
FluffyFTW <46359603+FluffyFTW@users.noreply.github.com>
2023-04-23 20:57:40 -0700

    🎨 Google loggin (#7)
    - Added Google credentials for the app

---
+++ demos/calendar_starter/calendar_starter.py
@@ -0,0 +1,10 @@
+from pprint import pprint
+from Google import Create_Service
+
+client_file_path = 'credentials.json'
+api_name = 'calendar'
+version = 'v3'
+scope = ['https://www.googleapis.com/auth/calendar']
+
+service = Create_Service(client_file_path, api_name, version, scope)
+pprint(service)
