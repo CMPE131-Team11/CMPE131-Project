a18eafcb038b531fed51538c6ee10f8e875a37f6
raf023 <rafaeljoshua19@gmail.com>
2023-04-25 20:18:39 -0700

    updated email_obj.py with classes

--- app/features/email_obj.py
+++ app/features/email_obj.py
@@ -5,21 +5,33 @@ from googleapiclient.discovery import build
 from requests import HTTPError
 
 class Send_email:
-    SCOPES = [
-            "https://www.googleapis.com/auth/gmail.send"
-        ]
-    flow = InstalledAppFlow.from_client_secrets_file('google_cred/credentials.json', SCOPES)
-    creds = flow.run_local_server(port=0)
-    service = build('gmail', 'v1', credentials=creds)
+    def __init__(self):
+        self.m_recipient = []
+        self.m_subject = []
+        self.m_body = []
+   
+    def recipient_text(self, p_recipient):
+        self.m_recipient = p_recipient
 
-    message = MIMEText('This is the body of the email')
-    message['to'] = 'rafael.antipuesto9@gmail.com' 
-    message['subject'] = 'Email Subject'
-    create_message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+    def body_text(self, p_body):
+        self.m_body = p_body
 
-    try:
-        message = (service.users().messages().send(userId="me", body=create_message).execute())
-        print(F'sent message to {message} Message Id: {message["id"]}')
-    except HTTPError as error:
-        print(F'An error occurred: {error}')
-        message = None
+    def subject_text(self, p_subject):
+        self.m_subject = p_subject
+
+    def send(self):
+        SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
+        flow = InstalledAppFlow.from_client_secrets_file('google_cred/credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+        service = build('gmail', 'v1', credentials=creds)
+        message = MIMEText(self.m_body)
+        message['to'] = self.m_recipient
+        message['subject'] = self.m_subject
+        create_message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+        try:
+            message = (service.users().messages().send(userId="me", body=create_message).execute())
+            print(F'sent message to {message} Message Id: {message["id"]}')
+        except HTTPError as error:
+            print(F'An error occurred: {error}')
+            message = None
