3920c04f3c0af07854765524a365faebcd1a5735
shambutennis <70358186+shambutennis@users.noreply.github.com>
2023-04-26 20:17:03 -0700

    :sparkles: Ketan use cases (#11)
    - Database model
    - Spam Emails
    - Edit Profile

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+myapp_obj = Flask(__name__)
+db = SQLAlchemy(myapp_obj)
+
+
+
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -2,3 +2,35 @@ import app.features.email_obj
 import features.tasks
 import features.calender
 import features.tasks
+from flask import session,request
+from models import db, User, Email
+from app import myapp_obj
+
+@myapp_obj.before_first_request
+def create_tables():
+    db.create_all()
+
+@myapp_obj.route("/addToSpam",methods = ['POST'])
+def add_to_spam():
+    email_id = request.form['email_id']
+    if email_id:
+        email = Email.query.get(email_id)
+        email.emailType = 'SPAM'
+        db.session.commit()
+
+
+@myapp_obj.route("/spam/")
+def show_spam():
+    spamList = Emails.query.filter(emailType = 'SPAM', recipient = session.get('username'))
+    return spamList
+
+@myapp_obj.route("/edit_profile", methods = ['POST'])
+def edit_user_profile():
+    user_id = session.get('user_id')
+    if user_id:
+        curr_user = user.query.get(user_id)
+        curr_user.username = request.form['username']
+        curr_user.password = request.form['password']
+        db.session.commit()
+    return
+    
