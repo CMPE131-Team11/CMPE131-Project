e9f9a85f1ae23c09451fdeaa6e1e8e39c08cdc31
raf023 <rafaeljoshua19@gmail.com>
2023-05-05 15:32:26 -0700

    Added the branch Raf-PrintEmail

---
+++ app/features/print_email.py
@@ -0,0 +1,86 @@
+from __future__ import print_function
+
+import os.path
+import time
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from bs4 import BeautifulSoup
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import pickle
+import os.path
+import base64
+import email
+from bs4 import BeautifulSoup
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        # Call the Gmail API
+        service = build('gmail', 'v1', credentials=creds)
+        #results = service.users().labels().list(userId='me').execute()
+        #labels = results.get('labels', [])
+
+        result = service.users().messages().list(userId='me').execute()
+        messages = result.get('messages')
+        if not messages: 
+            print("You have no new messages")
+        else:
+            #msg = service.users().messages().get(userId='me', id=message['id']).execute
+            for message in messages:
+                msg = service.users().messages().get(userId='me', id=message['id']).execute()
+                payload = msg['payload']
+                headers = payload['headers']
+                for d in headers:
+                    if d['name'] == 'Subject':
+                        subject = d['value']
+                    if d['name'] == 'From':
+                        sender = d['value']
+                parts = payload.get('parts')[0]
+                data = parts['body']['data']
+                data = data.replace("-","+").replace("_","/")
+                decoded_data = base64.b64decode(data)
+                soup = BeautifulSoup(decoded_data , "lxml")
+                body = soup.body()
+                print("Subject: ", subject)
+                print("From: ", sender)
+                print("Message: ", body)
+                print('\n')
+
+    except HttpError as error:
+        # TODO(developer) - Handle errors from gmail API.
+        print(f'An error occurred: {error}')
+
+
+if __name__ == '__main__':
+    main()
